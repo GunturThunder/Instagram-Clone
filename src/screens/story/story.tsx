cc7144a3d6a5e623e9c735faf5914ad982f81212
Guntur Angkasa Putra <gangkasa888@gmail.com>
2020-12-25 14:09:44 +0700

    timer

--- src/screens/story/story.tsx
+++ src/screens/story/story.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     SafeAreaView,
     StyleSheet,
@@ -10,17 +10,40 @@ import {
     TouchableOpacity,
     TextInput,
     KeyboardAvoidingView,
-    Platform
+    Platform,
+    Alert
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export const storyScreen = (props: any): React.ReactElement => {
+    const [counter, setCounter] = useState<number>(0);
+    const backButton = () =>{
+        setCounter(0),
+        props.navigation.goBack()
+        // clearInterval(timeCounter)
+    }
+    const timeCounter = () =>{
+        setTimeout(() => {
+            var i = 0
+            console.log(i+1)
+            // var a:string = new Date().getSeconds().toLocaleString()
+            // setCounter(counter+1)
+            // this.setState({
+            //   curTime : new Date().toLocaleString()
+            // })
+          }, 1000)
+    }
+    useEffect(() => {
+        timeCounter()
+    },[])
     return (
         <SafeAreaView style={styles.wrap}>
+            {/* {console.log('counter',counter)} */}
             <KeyboardAvoidingView
-                behavior={Platform.OS === "ios" ? "padding" : null}
+                behavior={Platform.OS === "ios" ? "padding" : null || undefined}
                 keyboardVerticalOffset={Platform.OS === "ios" ? 0 : -500}
                 style={styles.wrap}>
+                <Image style={{ position: 'absolute' }} source={require('../../assets/images/ibrahimovic.jpg')} />
                 <View style={styles.container}>
                     <View style={{ backgroundColor: '#959595', width: '100%' }}>
                         <View style={styles.timeCounterView}>
@@ -31,9 +54,14 @@ export const storyScreen = (props: any): React.ReactElement => {
                             <Image style={styles.images} source={require('../../assets/images/ibrahimovic.jpg')} />
                             <Text style={styles.textName}>ZlatanIbra</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity>
-                            <Icon size={16} color="#FFF" name="ellipsis-vertical" />
-                        </TouchableOpacity>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <TouchableOpacity>
+                                <Icon size={20} color="#FFF" name="ellipsis-horizontal" />
+                            </TouchableOpacity>
+                            <TouchableOpacity style={{ marginLeft: 5 }} onPress={backButton}>
+                                <Icon size={40} color="#FFF" name="close-outline" />
+                            </TouchableOpacity>
+                        </View>
                     </View>
                     <View style={{ flex: 1 }}>
 
@@ -42,7 +70,7 @@ export const storyScreen = (props: any): React.ReactElement => {
                         <View style={styles.textInput}>
                             <TextInput placeholderTextColor="#FFF" style={styles.textInputText} placeholder="Send message" />
                         </View>
-                        <TouchableOpacity>
+                        <TouchableOpacity style={{ marginRight: 5 }}>
                             <Icon size={30} name="ios-paper-plane-outline" color="#FFF" />
                         </TouchableOpacity>
                     </View>
@@ -98,7 +126,7 @@ const styles = StyleSheet.create({
         width: '85%'
     },
     textInputText: {
-        paddingVertical: '6%',
-        color:"#FFF"
+        paddingVertical: '5%',
+        color: "#FFF"
     }
 })
